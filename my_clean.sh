5a8584a2e228c260c43dcbcc02e28698b922a5e4
SHUBHAM <shubhamkumar50@yahoo.com>
2019-10-14 11:47:12 +0530

    Update my_clean.sh

--- my_clean.sh
+++ my_clean.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
-#function myclean {
     ## Show free space
     df -Th | grep -v fs
     # Will need English output for processing
     LANG=en_GB.UTF-8
 
     ## Clean apt cache
-    echo update-----------------------------
+    echo update----------------------------------------------------------------------------------------------------
+    echo                                   
+    
     apt-get update
-    echo install---------------------------------
+    
+    echo install----------------------------------------------------------------------------------------------------
     apt-get -f install
+    
+    echo autoremove--------------------------------------------------------------------------------------------------
     apt-get -y autoremove
+    
+    echo clean-------------------------------------------------------------------------------------------------------
     apt-get clean
 
+    echo ------------------------------------------Remove old versions of snap packages------------------------------------
     ## Remove old versions of snap packages
     snap list --all | while read snapname ver rev trk pub notes; do
         if [[ $notes = *disabled* ]]; then
             snap remove "$snapname" --revision="$rev"
         fi
     done
+    
+    echo ---------------------------------------------Set snap versions retain settings---------------------------------------
     ## Set snap versions retain settings
     if [[ $(snap get system refresh.retain) -ne 2 ]]; then snap set system refresh.retain=2; fi
     rm -f /var/lib/snapd/cache/*
 
+    echo --------------------------------------------Remove old versions of Linux Kernel-------------------------------------
     ## Remove old versions of Linux Kernel
     # This one-liner is deprecated since 18.04
     # dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs apt-get -y purge
@@ -39,4 +49,4 @@
 
     ## Show free space
     df -Th | grep -v fs
-#}
+
